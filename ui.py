80618235b7c3d295b6fd47e129c4e9b0262e2aef
PhatMap <74621545+PhatMap@users.noreply.github.com>
2023-05-15 23:45:27 +0700

    Add files via upload

--- ui.py
+++ ui.py
@@ -1,8 +1,7 @@
-from PyQt6.QtWidgets import QApplication, QSlider, QWidget, QPushButton, QFileDialog, QLabel, QHBoxLayout, QVBoxLayout, QSizePolicy, QComboBox
-from PyQt6.QtGui import QPixmap, QTransform, QIcon
+from PyQt6.QtWidgets import QApplication, QSlider, QWidget, QPushButton, QFileDialog, QLabel, QSizePolicy, QComboBox
+from PyQt6.QtGui import QPixmap, QIcon
 from PyQt6.QtCore import Qt
 from PyQt6.QtGui import QImage
-
 import sys, os
 import cv2
 import numpy as np
@@ -12,169 +11,101 @@ class FileBrowser(QWidget):
         super().__init__()
         self.initUI()
         self.image_path = None
-
+        self.k = None
+        self.image = None
+        self.cimage = None
     
 
     def initUI(self):
+        self.banner_label1 = QLabel(self)
+        banner_pixmap = QPixmap("banner1.png")
+        self.banner_label1.setPixmap(banner_pixmap)
+        self.banner_label1.setFixedSize(1050, 200)
+        self.banner_label1.move(0, 0)
         
-
-        # Banner label
-        self.banner_label = QLabel(self)
-        # Load the banner pixmap
-        banner_pixmap = QPixmap("banner.png")
-        # Scale the banner pixmap to the window's width while maintaining its aspect ratio        
-        # Set the scaled banner pixmap to the banner label
-        self.banner_label.setPixmap(banner_pixmap)
-        # Horizontal layout for banner and text
-        header_layout = QHBoxLayout()
-        header_layout.addWidget(self.banner_label)
+        self.banner_label2 = QLabel(self)
+        banner_pixmap = QPixmap("banner2.png")
+        self.banner_label2.setPixmap(banner_pixmap)
+        self.banner_label2.setFixedSize(500, 200)
+        self.banner_label2.move(1040, 0)
 
         # File label
         self.file_label = QLabel('', self)
         self.file_label.setSizePolicy(QSizePolicy.Policy.Preferred, QSizePolicy.Policy.Fixed)
         self.file_label.setAlignment(Qt.AlignmentFlag.AlignCenter)
-        self.banner_label.setScaledContents(True) 
-
+        self.banner_label1.setScaledContents(True) 
+        self.banner_label2.setScaledContents(True) 
         # Image label
         self.image_label = QLabel(self)
-        self.image_label.move(20, 60)
-        self.image_label.setFixedWidth(600)
-        self.image_label.setFixedHeight(300)
-        self.image_label.setText("Choose your image to start")
+        self.image_label.move(5, 210)
+        self.image_label.setFixedSize(750, 500)
+        self.image_label.setScaledContents(True) 
         self.image_label.setAlignment(Qt.AlignmentFlag.AlignCenter)
 
         # Image label
         self.image_label_after = QLabel(self)
-        self.image_label_after.move(420, 80)
-        self.image_label_after.setFixedWidth(600)
-        self.image_label_after.setFixedHeight(300)
-        self.image_label_after.setText("Choose your image to start")
+        self.image_label_after.move(780, 210)
+        self.image_label_after.setFixedSize(750, 500)
+        self.image_label_after.setScaledContents(True) 
         self.image_label_after.setAlignment(Qt.AlignmentFlag.AlignCenter)
 
-        # Layout for the file label and buttons
-        hbox = QHBoxLayout()
-        hbox.addWidget(self.file_label)
-
-        button_style = """
-            QPushButton {
-                background-color: white;
-                color: black;
-                border: none;
-                padding: 8px 16px;
-                border-radius: 8px;
-                font-size: 16px;
-                font-weight: bold;
-            }
-            QPushButton:hover {
-                background-color: #F5F5F5;
-            }
-            QPushButton:pressed {
-                background-color: #E8E8E8;
-            }
-            QPushButton:disabled {
-                background-color: #D3D3D3;
-                color: #A0A0A0;
-                border: none;
-            }
-        """
         # Replace Origin and Save As button Layout
-        save_as_layout = QHBoxLayout()
+        
         self.slider_label = QLabel(self)
+        self.slider_label.setFixedSize(60, 50)
+        self.slider_label.setScaledContents(True) 
+        self.slider_label.move(480, 740)
         
         self.slider = QSlider(Qt.Orientation.Horizontal, self)
-        self.slider.setMinimum(1)
-        self.slider.setMaximum(5)
+        self.slider.setMinimum(0)
+        self.slider.setMaximum(100)
+        self.slider.setFixedWidth(250)
         self.slider.setTickPosition(QSlider.TickPosition.TicksBelow)
-        self.slider.valueChanged.connect(self.slide)
-        self.slider.valueChanged.connect(self.combo)
-        self.slider.setEnabled(False)
+        self.slider.setHidden(True)
+        self.slider.move(550, 750)
         
         self.replace_button = QPushButton('Replace Origin', self)
         self.replace_button.clicked.connect(self.replace_origin)
-        self.replace_button.setStyleSheet(button_style)
         self.replace_button.setSizePolicy(QSizePolicy.Policy.Preferred, QSizePolicy.Policy.Fixed)
+        self.replace_button.setStyleSheet("background-color: #ffb38a;")
+        self.replace_button.move(1300, 750)
         
         self.save_as_button = QPushButton('Save as', self)
         self.save_as_button.clicked.connect(self.save_as)
-        self.save_as_button.setStyleSheet(button_style)
         self.save_as_button.setSizePolicy(QSizePolicy.Policy.Preferred, QSizePolicy.Policy.Fixed)
+        self.save_as_button.setStyleSheet("background-color: #ffb38a;")
+        self.save_as_button.move(1400, 750)
         
-        save_as_layout.addWidget(self.slider_label, alignment=Qt.AlignmentFlag.AlignBottom)
-        save_as_layout.addWidget(self.slider, alignment=Qt.AlignmentFlag.AlignCenter)
-        save_as_layout.addWidget(self.replace_button)
-        save_as_layout.addWidget(self.save_as_button)
 
         # Add four buttons to the layout
         self.button1 = QPushButton('Browse', self)
         self.button1.clicked.connect(self.showDialog)
+        self.button1.setStyleSheet("background-color: #ffb38a;")
+        self.button1.move(50, 750)
         
         #Them chuc nang o day
         self.comboBox = QComboBox(self)
-        self.comboBox.addItems(["Gaussian", "Max", "Min"])
+        self.comboBox.addItems(["","Gaussian", "Max", "Min","Median","Reduce noise","Sharpening","Rotation","grayscale"])
+        self.comboBox.activated[int].connect(self.combo)
         self.comboBox.setSizePolicy(QSizePolicy.Policy.Preferred, QSizePolicy.Policy.Fixed)
+        self.comboBox.setStyleSheet("background-color: #ffb38a;")
+        self.comboBox.setFixedWidth(200)
+        self.comboBox.move(170, 750)
         
-        self.submit = QPushButton('Submit', self)
-        self.submit.clicked.connect(self.combo)
-        self.submit.setSizePolicy(QSizePolicy.Policy.Preferred, QSizePolicy.Policy.Fixed)
 
+        self.slider.valueChanged.connect(self.slide)
+        
+        
         # Disable the first time user open the app
         self.comboBox.setEnabled(False)
-        self.submit.setEnabled(False)
         self.save_as_button.setEnabled(False)
         self.replace_button.setEnabled(False)
 
-        # Add a label for Browse layout
-        browse_label = QLabel('Step 1: Choose your file', self)
-        browse_label.setAlignment(Qt.AlignmentFlag.AlignCenter)
-        browse_label.setStyleSheet("color: white;")
-        
-        # Layout for the Browse button
-        browse_layout = QVBoxLayout()
-        browse_layout.addWidget(browse_label)
-        browse_layout.addWidget(self.button1)
 
-        # Add a label for Browse layout
-        filter_label = QLabel('Step 2: Apply the filter', self)
-        filter_label.setAlignment(Qt.AlignmentFlag.AlignCenter)
-        filter_label.setStyleSheet("color: white;")
 
-        # Layout for Gaussian, Max, and Min buttons
-        button_layout = QHBoxLayout()
-        button_layout.addWidget(self.comboBox)
-        button_layout.addWidget(self.submit)
-        
 
-        # Layout for filter with label
-        filter_layout = QVBoxLayout()
-        filter_layout.addWidget(filter_label)
-        filter_layout.addLayout(button_layout)
 
-        # Add the Browse layout and the button layout to a horizontal layout
-        hbox2 = QHBoxLayout()
-        hbox2.addLayout(browse_layout)
-        hbox2.addLayout(filter_layout)
-        hbox2.addLayout(save_as_layout)
-        hbox2.setSpacing(10)
 
-        # Create a horizontal layout for the image labels
-        image_layout = QHBoxLayout()
-        image_layout.addWidget(self.image_label)
-        image_layout.addLayout(save_as_layout)
-        image_layout.addWidget(self.image_label_after)
-
-        # Set the layout for the window
-        vbox = QVBoxLayout()
-        vbox.addLayout(header_layout) # Add the header layout
-        vbox.addLayout(hbox)
-        vbox.addLayout(image_layout)
-        vbox.addLayout(hbox2)
-        vbox.setStretch(0, 1)
-        vbox.setStretch(1, 2)
-        vbox.setStretch(2, 4)
-        vbox.setStretch(3, 4)
-        vbox.setStretch(4, 1)
-        vbox.setSpacing(20)
-        self.setLayout(vbox)
 
 
         # Set the default image
@@ -185,43 +116,47 @@ class FileBrowser(QWidget):
         self.image_label_after.setPixmap(scaled_pixmap)
 
 
-
+        self.setWindowIcon(QIcon('camera.png'))
+        self.setStyleSheet("background-color: #ADD8E6 ;")
         self.showMaximized()
         self.setWindowTitle('Final project')
         self.show()
     
     
     def slide(self):
-        
         self.slider_label.setText("Value: "+str(self.sender().value()))
     
     def combo(self):
-        self.slider.setEnabled(True)
-        print(self.sender().value())
-        if self.comboBox.currentIndex()==0:
-            self.gaussian_filter(self.sender().value())
-        elif self.comboBox.currentIndex()==1:
-            self.max_filter()
+        self.slider.setValue(0)
+        self.slider.setHidden(True)
+        if self.comboBox.currentIndex()==1:
+            self.slider.setHidden(False)
+            self.gaussian_filter()
+            self.slider.valueChanged[int].connect(self.gaussian_filter)
         elif self.comboBox.currentIndex()==2:
+            self.slider.setHidden(False)
+            self.max_filter()
+            self.slider.valueChanged[int].connect(self.max_filter)
+        elif self.comboBox.currentIndex()==3:
+            self.slider.setHidden(False)
             self.min_filter()
-    
-    def min_filter(self):
-        if not self.image_path:
-            return
-        try:
-            image = cv2.imread(self.image_path, cv2.IMREAD_GRAYSCALE)
-            if image is None:
-                return
-            kernel = np.ones((5, 5), np.uint8)
-            erosion = cv2.erode(image, kernel, iterations=1)
-            qimg = QImage(erosion.data, erosion.shape[1], erosion.shape[0], QImage.Format.Format_RGB888)
-            pixmap = QPixmap.fromImage(qimg)
-            scaled_pixmap = pixmap.scaledToHeight(self.image_label_after.height())
-            self.image_label_after.setPixmap(scaled_pixmap)
-        except cv2.error as e:
-            print(f"Error processing image: {e}")
-
-
+            self.slider.valueChanged[int].connect(self.min_filter)
+        elif self.comboBox.currentIndex()==4:
+            self.slider.setHidden(False)
+            self.medianBlur()
+            self.slider.valueChanged[int].connect(self.medianBlur)
+        elif self.comboBox.currentIndex()==5:
+            self.reduce_noise()
+        elif self.comboBox.currentIndex()==6:
+            self.sharpening()
+        elif self.comboBox.currentIndex()==7:
+            self.slider.setMaximum(360)
+            self.slider.setHidden(False)
+            self.rotation()
+            self.slider.valueChanged[int].connect(self.rotation)
+        elif self.comboBox.currentIndex()==8:
+            self.grayscale()
+            
     def save_as(self):
         if not self.image_path:
             return
@@ -241,7 +176,7 @@ class FileBrowser(QWidget):
     def replace_origin(self):
         if not self.image_path:
             return
-
+        self.image =self.cimage
         # Get the pixmap from the image_label_after
         pixmap_after = self.image_label_after.pixmap()
 
@@ -254,52 +189,108 @@ class FileBrowser(QWidget):
             scaled_default_pixmap = default_pixmap.scaledToHeight(self.image_label_after.height())
             self.image_label_after.setPixmap(scaled_default_pixmap)
 
-
-
-    def max_filter(self):
-        if not self.image_path:
-            return
-        image = cv2.imread(self.image_path, cv2.IMREAD_GRAYSCALE)
-        if image is None:
-            return
-        kernel = np.ones((5, 5), np.uint8)
-        dilation = cv2.dilate(image, kernel, iterations=1)
-        dilation_rgb = cv2.cvtColor(dilation, cv2.COLOR_GRAY2RGB) # Convert to RGB color space
-        qimg = QImage(dilation_rgb.data, dilation_rgb.shape[1], dilation_rgb.shape[0], QImage.Format.Format_RGB888)
+    def min_filter(self,k=0):
+        gray = cv2.cvtColor(self.image, cv2.COLOR_BGR2GRAY)
+        kernel = np.ones((k, k), np.uint8)
+        erosion = cv2.erode(gray, kernel, iterations=1)
+        img = cv2.cvtColor(erosion, cv2.COLOR_GRAY2RGB)
+        erosion_rgb = cv2.resize(img, (1640, 900))
+        qimg = QImage(erosion_rgb.data, erosion_rgb.shape[1], erosion_rgb.shape[0], QImage.Format.Format_BGR888)
         pixmap = QPixmap.fromImage(qimg)
         scaled_pixmap = pixmap.scaledToHeight(self.image_label_after.height())
         self.image_label_after.setPixmap(scaled_pixmap)
+        self.cimage  = erosion_rgb
 
-    
-    def gaussian_filter(self, k = 5):
-        if not self.image_path:
-            return
-        image = cv2.imread(self.image_path)
-        image = cv2.resize(image, (300,300))
-        if image is None:
-            return
-        gaussian = cv2.GaussianBlur(image, (k, k), 0)
-        qimg = QImage(gaussian.data, gaussian.shape[1], gaussian.shape[0], QImage.Format.Format_RGB888)
+    def max_filter(self,k=0):
+        gray = cv2.cvtColor(self.image, cv2.COLOR_BGR2GRAY)
+        kernel = np.ones((k, k), np.uint8)
+        dilation = cv2.dilate(gray, kernel, iterations=1)
+        img = cv2.cvtColor(dilation, cv2.COLOR_GRAY2RGB) # Convert to RGB color space
+        dilation_rgb = cv2.resize(img, (1640, 900))
+        qimg = QImage(dilation_rgb.data, dilation_rgb.shape[1], dilation_rgb.shape[0], QImage.Format.Format_BGR888)
         pixmap = QPixmap.fromImage(qimg)
         scaled_pixmap = pixmap.scaledToHeight(self.image_label_after.height())
         self.image_label_after.setPixmap(scaled_pixmap)
-        self.image_label_after.adjustSize() #Thêm dòng này để hiển thị đúng kích thước ảnh
+        self.cimage  = dilation_rgb
 
     
-    def show_image_after(self, image):
-        if len(image.shape) == 2:  # Grayscale image
-            qimage = QImage(image.data, image.shape[1], image.shape[0],QImage.Format.Format_Grayscale8)
-        else:  # Color image
-            image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-            qimage = QImage(image.data, image.shape[1], image.shape[0], QImage.Format.Format_RGB888)
-        pixmap = QPixmap(qimage)
+    def gaussian_filter(self,k=1):
+        if k%2==0:
+            k+=1
+        img = cv2.GaussianBlur(self.image, (k, k), 0)
+        gaussian = cv2.resize(img, (1640, 900))
+        qimg = QImage(gaussian.data, gaussian.shape[1], gaussian.shape[0], QImage.Format.Format_BGR888)
+        pixmap = QPixmap.fromImage(qimg)
         scaled_pixmap = pixmap.scaledToHeight(self.image_label_after.height())
         self.image_label_after.setPixmap(scaled_pixmap)
+        self.image_label_after.adjustSize()
+        self.cimage  = gaussian
+ 
+    def medianBlur(self,k=1):
+        if k%2==0:
+            k+=1
+        img=cv2.cvtColor(self.image, cv2.COLOR_BGR2RGB)
+        img = cv2.medianBlur(img,k)
+        
+        image = cv2.resize(img, (1640, 900))
+        qimg = QImage(image.data, image.shape[1], image.shape[0], QImage.Format.Format_RGB888)
+        pixmap = QPixmap.fromImage(qimg)
+        scaled_pixmap = pixmap.scaledToHeight(self.image_label_after.height())
+        self.image_label_after.setPixmap(scaled_pixmap)
+        self.image_label_after.adjustSize()
+        self.cimage  = image
+        
+    def reduce_noise(self):
+        detect = cv2.fastNlMeansDenoisingColored(self.image,None,20,10,7,21)
+        
+        image = cv2.resize(detect, (1640, 900))
+        qimg = QImage(image.data, image.shape[1], image.shape[0], QImage.Format.Format_BGR888)
+        pixmap = QPixmap.fromImage(qimg)
+        scaled_pixmap = pixmap.scaledToHeight(self.image_label_after.height())
+        self.image_label_after.setPixmap(scaled_pixmap)
+        self.image_label_after.adjustSize()
+        self.cimage  = image
+        
+    def sharpening(self):
+        kernel_sharpening = np.array([[-1,-1,-1], 
+                              [-1,9,-1], 
+                              [-1,-1,-1]])
+        sharpened = cv2.filter2D(self.image, -1, kernel_sharpening)
+        image = cv2.resize(sharpened, (1640, 900))
+        qimg = QImage(image.data, image.shape[1], image.shape[0], QImage.Format.Format_BGR888)
+        pixmap = QPixmap.fromImage(qimg)
+        scaled_pixmap = pixmap.scaledToHeight(self.image_label_after.height())
+        self.image_label_after.setPixmap(scaled_pixmap)
+        self.image_label_after.adjustSize()
+        self.cimage  = image
+        
+    def rotation(self,k=0):
+        rows,cols = self.image.shape[:2] 
+        M = cv2.getRotationMatrix2D((cols/2,rows/2),k,1) 
+        dst = cv2.warpAffine(self.image,M,(cols,rows))
+        image = cv2.resize(dst, (1640, 900))
+        qimg = QImage(image.data, image.shape[1], image.shape[0], QImage.Format.Format_BGR888)
+        pixmap = QPixmap.fromImage(qimg)
+        scaled_pixmap = pixmap.scaledToHeight(self.image_label_after.height())
+        self.image_label_after.setPixmap(scaled_pixmap) 
+        self.image_label_after.adjustSize()
+        self.cimage  = image
+    
+    def grayscale(self):
+        gray = cv2.cvtColor(self.image, cv2.COLOR_BGR2GRAY)
+        image = cv2.resize(gray, (1640, 900))
+        qimg = QImage(image.data, image.shape[1], image.shape[0], QImage.Format.Format_Grayscale8)
+        pixmap = QPixmap.fromImage(qimg)
+        scaled_pixmap = pixmap.scaledToHeight(self.image_label_after.height())
+        self.image_label_after.setPixmap(scaled_pixmap)
+        self.cimage  = image
+        
 
     def showDialog(self):
         fname, _ = QFileDialog.getOpenFileName(self, 'Open file', '/Users/asus/OneDrive/Pictures', 'Images (*.png *.jpg)')
         if fname:
             self.image_path = fname
+            self.image = cv2.imread(self.image_path)
             pixmap = QPixmap(fname)
             scaled_pixmap = pixmap.scaledToHeight(self.image_label.height())
             self.image_label.setPixmap(scaled_pixmap)
@@ -307,7 +298,6 @@ class FileBrowser(QWidget):
 
             # Enable back buttons
             self.comboBox.setEnabled(True)
-            self.submit.setEnabled(True)
             self.save_as_button.setEnabled(True)
             self.replace_button.setEnabled(True)
 
